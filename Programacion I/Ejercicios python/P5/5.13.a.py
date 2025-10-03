97da9858d22e7a7d0524db8e1c45bf4d2dbe4d6c
Federico Winter <winterfedericoexe@gmail.com>
2025-10-02 22:36:57 -0300

    cambio nombres carpeta

--- Programacion I/Ejercicios python/P5/5.13.a.py
+++
@@ -1,9 +0,0 @@
-def dict_notas(alumnos: list[tuple[str, int]]) -> dict[str, int]:
-    notas = {}
-
-    for alumno in alumnos:
-        nombre = alumno[0]
-        nota = alumno[1]
-        notas[nombre] = nota
-    
-    return notas
