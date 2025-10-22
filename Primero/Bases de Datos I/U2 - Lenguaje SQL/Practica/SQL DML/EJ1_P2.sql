2988e4692f06bb7501007a1bb140c4e664df3f35
Federico Winter <winterfedericoexe@gmail.com>
2025-10-21 22:51:05 -0300

    Avance en BDDI - Practica 2 - parte 2

--- Primero/Bases de Datos I/U2 - Lenguaje SQL/Practica/SQL DML/EJ1_P2.sql
+++ Primero/Bases de Datos I/U2 - Lenguaje SQL/Practica/SQL DML/EJ1_P2.sql
@@ -1,3 +1,49 @@
+USE empresa_basica
+
+----------------------- a
+
 SELECT nombre, apellido1, apellido2, dni
 FROM DIRECTOR
 WHERE dni = 33193456
+
+SELECT nombre, apellido1 + ' ' + apellido2 AS APELLIDO, dni
+FROM DIRECTOR
+WHERE dni = 33193456
+
+----------------------- b
+
+SELECT 
+	dptoid AS NUMERO_DEPARTAMENTO,
+	COUNT(*) AS CANTIDAD_PROY
+FROM PROYECTO
+GROUP BY dptoid
+
+----------------------- c
+
+SELECT 
+	TOP 1
+	nombre, dni, sueldo
+FROM EMPLEADO
+WHERE dptoid = 1
+ORDER BY sueldo DESC
+
+----------------------- d
+
+SELECT 
+	AVG(sueldo)
+FROM EMPLEADO
+WHERE dptoid = 1
+
+----------------------- e (ver subquery)
+
+SELECT E.nombre, E.apellido1, P.horas, P.proyectoid
+FROM PROYECTO_EMPLEADO AS P
+INNER JOIN
+	EMPLEADO AS E ON P.empleadoid = E.empleadoid
+WHERE P.horas > 3 AND P.proyectoid = 1
+
+----------------------- f
+
+SELECT nombre, apellido1, fechanac
+FROM EMPLEADO
+WHERE fechanac <= '1980-12-31' and fechanac >= '1960-01-01'
