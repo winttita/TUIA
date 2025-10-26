b83d0e11fff71787e1e7ada9c3ab5ec87c3d7304
Federico Winter <winterfedericoexe@gmail.com>
2025-10-26 20:18:57 -0300

    Avances en PII: TADp1. Ejercicio 8 terminado.

--- Primero/Programacion II/U4 - TADs/tad_p1.py
+++ Primero/Programacion II/U4 - TADs/tad_p1.py
@@ -1,6 +1,18 @@
 ## Ejercicio 1 ############################################################################################################################################
 
 from typing import Any
+import math
+
+def es_primo(x: int) -> bool:
+        if not isinstance(x, int) or x <= 1:
+            return False
+        elif x == 2:
+            return True
+        else:
+            for i in range(2,  x):
+                if x % i == 0:
+                    return False
+            return True
 
 class _Nodo:
     def __init__(self, dato: Any = None, prox=None) -> None:
@@ -193,4 +205,190 @@ class ListaEnlazada:
                 invert.insert(0, act.dato)
                 act = act.prox
         self.prim = invert.prim
+        return
+    
+## Ejercicio 7 ############################################################################################################################################
+
+    def filtrar_primos(self) -> 'ListaEnlazada':
+        primos = ListaEnlazada()
+        act = self.prim
+        ult = None
+        while act is not None:
+            if es_primo(act.dato):
+                nuevo = _Nodo(act.dato)
+                if primos.prim is None:
+                    primos.prim = nuevo
+                    ult = nuevo
+                else:
+                    ult.prox = nuevo
+                    ult = nuevo
+                primos.len += 1
+            act = act.prox
+        return primos
+    
+## Ejercicio 8 ############################################################################################################################################
+
+    def insertar_palabra_despues(self, l: 'ListaEnlazada', palabra_objetivo: str, palabra_insertar: str) -> 'ListaEnlazada':
+        l2 = ListaEnlazada()
+        act = l.prim
+        ult = None
+        pos = 0
+        while act is not None:
+            if act.dato.upper() == "HOLA":
+                nuevo = _Nodo("Mundo")
+                if l2.prim is None:
+                    l2.prim = nuevo
+                else:
+            elif l2.prim is None:
+                nuevo = _Nodo(act.dato)
+                l2.prim = nuevo
+            else:
+                pass
+            act = act.prox
+
+
+## Ejercicio 6 ############################################################################################################################################
+class _Nodo2:
+    def __init__(self, dato: Any = None, prox = None, ant = None) ->None:
+        self.dato = dato
+        self.prox = prox
+        self.ant = ant
+
+    def __str__(self) -> str:
+        return str(self.dato)
+
+    def ver_lista(nodo) -> None:
+        """Recorre todos los nodos a través de sus enlaces, mostrando sus
+        contenidos.
+        """
+        while nodo is not None:
+            print(nodo)
+            nodo = nodo.prox
+
+class ListaDoblementeEnlazada:
+    """Modela una lista DOBLEMENTE enlazada."""
+
+    def __init__(self) -> None:
+        """Crea una lista enlazada vacía."""
+        # Referencia al primer nodo (None si la lista está vacía)
+        self.prim = None
+        # Cantidad de elementos de la lista
+        self.len = 0
+        #Referencia al ultimo elemento de la lista (None si esta vacia)
+        self.ult = None
+
+    def insert(self, i: int, x: Any) -> None:
+        """Inserta el elemento x en la posición i.
+        Si la posición es inválida, imprime un error y retorna inmediatamente.
+        """
+        if i < 0 or i > self.len:
+            print("Posición inválida")
+            return
+        nuevo = _Nodo2(x)
+        if i == 0:
+            if self.len == 0:
+                self.prim = self.ult = nuevo
+                return
+            else:
+                self.ult = nuevo
+                return
+
+            
+    def pop(self, i: int | None = None) -> Any:
+        """Elimina el nodo de la posición i, y devuelve el dato contenido.
+        Si i está fuera de rango, se muestra un mensaje de error y se
+        retorna inmediatamente. Si no se recibe la posición, devuelve el
+        último elemento.
+        """
+        if i is None:
+            i = self.len - 1
+        if i < 0 or i >= self.len:
+            print(" Posición inválida ")
+            return
+        if i == 0:
+            # Caso particular: saltear la cabecera de la lista
+            dato = self.prim.dato
+            self.prim = self.prim.prox
+        else:
+            # Buscar los nodos en las posiciones (i -1) e (i)
+            n_ant = self.prim
+            n_act = n_ant.prox
+            for pos in range(1, i):
+                n_ant = n_act
+                n_act = n_ant.prox
+            # Guardar el dato y descartar el nodo
+            dato = n_act.dato
+            n_ant.prox = n_act.prox
+            self.len -= 1
+        return dato
+
+    def remove(self, x: Any) -> None:
+        """Borra la primera aparición del valor x en la lista.
+        Si x no está en la lista, imprime un mensaje de error y retorna
+        inmediatamente.
+        """
+        if self.len == 0:
+            print("La lista esta vacía")
+            return
+        if self.prim.dato == x:
+            # Caso particular: saltear la cabecera de la lista
+            self.prim = self.prim.prox
+        else:
+            # Buscar el nodo anterior al que contiene a x (n_ant)
+            n_ant = self.prim
+            n_act = n_ant.prox
+            while n_act is not None and n_act.dato != x:
+                n_ant = n_act
+                n_act = n_ant.prox
+            if n_act is None:
+                print("El valor no está en la lista.")
+                return
+            # Descartar el nodo
+            n_ant.prox = n_act.prox
+            self.len -= 1
+
+    def __len__(self) -> int:
+        """
+        Muestra la longitud de la lista
+        """
+        return self.len
+
+    def __str__(self) -> str:
+        """
+            Muestra el contenido de la lista enlazada.
+        """
+        ret = '['
+        n_ant = self.prim
+        for no in range(self.len):
+            ret += str(n_ant.dato) + ', '
+
+        ret = ret[:-2] + ']'
+
+        return ret
+        
+    def index(self, x: Any) -> int:
+        nodo = self.prim
+        i = 0
+        while nodo is not None:
+            if nodo.dato == x:
+                return  i
+            i += 1
+            nodo = nodo.prox
+        return -1
+
+    def append(self, x: Any) -> None:
+        n_ant = self.prim
+        if n_ant is None:
+            n_nuevo = _Nodo2(x)
+            self.prim = n_nuevo
+            self.len += 1
+            return
+        n_nuevo = _Nodo(x)
+        self.ult.prox = n_nuevo
+        self.len += 1
+        return
+
+    def extend(self, lista: 'ListaDoblementeEnlazada') -> None:
+        self.ult.prox = lista.prim
+        self.len += lista.len
         return
